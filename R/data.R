ec58b8a96456fd0e7d4b6eb90f52c4ae19ac588b
teatree1212 <a.eckes@posteo.net>
2023-08-21 17:27:57 +0200

    update dataset and example to reflect relative water content as simulated by Bijou

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Example forcing data and parameters needed for running the DH model
-#' @format contains lists with data frames containing forcing data for three sites in France, alongside a parameter with best, upper and lower limits for the parameters
-#' @source Data obtained from Cyrille Rathgeber, and modified to weekly resolution afterwards.
+#' @format contains lists with data frames containing forcing data for two sites in France, alongside a parameter with best, upper and lower limits for the parameters
+#' @source Data obtained from Cyrille Rathgeber,and used in http://www.plantphysiol.org/lookup/doi/10.1104/pp.16.00037, and modified to weekly resolution afterwards.
 #' Cpool was obtained from Ogee et al Figure 3, and scaled for the purposes here. These are site data from locations where xylogenesis and wood anatomical data exists.
 #'
 #' @examples
